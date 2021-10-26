7db5a0de5a5c2ad3852c2d03aef71e98972dfbc8
BaBaSaMa <19013345@myrp.edu.sg>
2021-10-26 20:14:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ sudo forever start ./API/app.js
 sudo forever start ./Expo-Notification-Test/app.js
 sudo forever start ./Phantom/app.js
 sudo forever start ./Portfolio/app.js
+sudo forever start ./Storage/app.js
 
 sudo forever list
 
