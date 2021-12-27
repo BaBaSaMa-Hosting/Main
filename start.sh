4d80b10359ef3f4f22b14ac551e15fdb5fdad968
BaBaSaMa <19013345@myrp.edu.sg>
2021-12-27 19:40:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@ echo "Starting Script"
 
 sudo forever start ./app.js
 sudo forever start ./API/app.js
-sudo forever start ./Expo-Notification-Test/app.js
 sudo forever start ./Phantom/app.js
-sudo forever start ./Portfolio/app.js
 sudo forever start ./Storage/app.js
 
 sudo forever list
