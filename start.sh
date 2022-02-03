921547ebfeac7b3845fff016302ede35f7acbcb7
BaBaSaMa <19013345@myrp.edu.sg>
2022-02-03 20:47:25 +0800

    update home management

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ sudo forever start ./app.js
 sudo forever start ./API/app.js
 sudo forever start ./Phantom/app.js
 sudo forever start ./Storage/app.js
+sudo forever start ./Home-Management/Home-Management-Web/app.js
 
 sudo forever list
 
