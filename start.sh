72614182fcc21978132fb7b5e05d9e442c05af5e
BaBaSaMa <19013345@myrp.edu.sg>
2022-02-14 23:07:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ sudo forever start ./app.js
 sudo forever start ./API/app.js
 sudo forever start ./Phantom/app.js
 sudo forever start ./Storage/app.js
-sudo forever start ./Home-Management/Home-Management-Web/app.js
 
 sudo forever list
 
